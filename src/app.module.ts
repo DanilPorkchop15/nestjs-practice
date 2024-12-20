152dfac93b2ea03ac75f9c7ec5affc60fad26f8c
Danil Kostin <d.kostin@worksolutions.ru>
2024-12-20 17:51:37 +0300

    feat: add orm and config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { FilesModule } from './modules/files/files.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      logger: 'advanced-console',
+    }),
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
